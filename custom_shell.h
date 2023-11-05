10d9d6cb97a9e2a44906fd8dae2d58326e012d7a
iintore <tonylewismanzi@gmail.com>
2023-11-05 01:33:24 -0800

    Header commit

--- custom_shell.h
+++ custom_shell.h
@@ -1,21 +1,15 @@
-#ifndef CUSTOM_SHELL_H
-#define CUSTOM_SHELL_H
+#ifndef "CUSTOM_SHELL_H"
+#define "CUSTOM_SHELL_H"
 
-/* Required header files */
+#define MAX_COMMAND_LENGTH 100
+
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <limits.h>
 
-/* Prototypes for functions in the change_dir.c file */
-void move_up_directory(data_shell *data);
-void move_to_directory(data_shell *data);
-void move_to_previous(data_shell *data);
-void move_to_home(data_shell *data);
+int main(void);
+
 #endif
