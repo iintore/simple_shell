04ab78f7f4a2177cce4b71e9e516833c4b273c36
Ana123tase <anastasedukunda@gmail.com>
2023-11-06 10:52:23 -0800

    uncorrected task4

--- task4.c
+++ task4.c
@@ -12,97 +12,88 @@
  * main - Simple Shell 0.4
  *
  * Description:
- * This is a basic shell that handles command lines with arguments,
- * includes PATH support, avoids forking if the command doesn't exist,
- * and implements the exit built-in command to exit the shell gracefully.
+ * This is a basic shell that handles command lines with args.
  *
  * Return: 0 on success, -1 on error.
  */
 int main(void)
 {
-    char input[MAX_INPUT_LENGTH];     /* Stores user input */
-    char *arguments[MAX_ARGUMENTS];   /* Stores command and arguments */
-    int argc;                         /* Argument count */
-
-    while (1)
-    {
-        printf(":) ");    /* Display shell prompt */
-
+	char input[MAX_INPUT_LENGTH];     /* Stores user input */
+	char *arguments[MAX_ARGUMENTS];   /* Stores command and arguments */
+	int argc;                         /* Argument count */
+	
+	while (1)
+	{
+		printf(":) ");    /* Display shell prompt */
         /* Read user input */
-        fgets(input, sizeof(input), stdin);
-
-        if (feof(stdin))
-        {
-            break;  /* Exit on Ctrl+D (EOF) */
-        }
-
-        /* Remove the newline character from the input */
-        input[strcspn(input, "\n")] = '\0';
+		fgets(input, sizeof(input), stdin);
+		
+		if (feof(stdin))
+		{
+			break;  /* Exit on Ctrl+D (EOF) */
+		}
+		/* Remove the newline character from the input */
+		input[strcspn(input, "\n")] = '\0';
 
         /* Check if the command is "exit" to exit the shell */
-        if (strcmp(input, "exit") == 0)
-        {
-            printf("Murabeho!\n");  /* Display a farewell message */
-            break;  /* Exit the shell gracefully */
-        }
-
-        // Check if the command exists in PATH
-        char *path = getenv("PATH");
-        char *command = strtok(input, " ");
-
-        if (access(command, X_OK) == 0)
-        {
-            strcpy(input, command);
-        }
-        else if (path != NULL)
-        {
-            char *path_token = strtok(path, ":");
-            while (path_token != NULL)
-            {
-                char cmd_path[MAX_INPUT_LENGTH];
-                snprintf(cmd_path, sizeof(cmd_path), "%s/%s", path_token, command);
-                if (access(cmd_path, X_OK) == 0)
-                {
-                    strcpy(input, cmd_path);
-                    break;
-                }
-                path_token = strtok(NULL, ":");
-            }
-        }
-
-        pid_t pid = fork();  /* Create a child process */
-
-        if (pid == -1)
-        {
-            perror("Error: Unable to create a child process.");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            /* Child process */
-            argc = 0;
-            arguments[argc] = strtok(input, " ");  /* Split input into tokens */
-
-            while (arguments[argc] != NULL)
-            {
-                argc++;
-                arguments[argc] = strtok(NULL, " ");
-            }
-            arguments[argc] = NULL;
-
-            if (execvp(arguments[0], arguments) == -1)
-            {
-                perror("Execution error");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent process */
-            int status;
-            wait(&status);  /* Wait for child process to complete */
-        }
-    }
-
-    return 0;  /* Return 0 on success */
+		if (strcmp(input, "exit") == 0)
+		{
+			printf("Murabeho!\n");  /* Display a farewell message */
+			break;  /* Exit the shell gracefully */
+		}
+		char *path = getenv("PATH");
+		char *command = strtok(input, " ");
+		
+		if (access(command, X_OK) == 0)
+		{
+			strcpy(input, command);
+		}
+		else if (path != NULL)
+		{
+			char *path_token = strtok(path, ":");
+			
+			while (path_token != NULL)
+			{
+				char cmd_path[MAX_INPUT_LENGTH];
+				snprintf(cmd_path, sizeof(cmd_path), "%s/%s", path_token, command);
+				if (access(cmd_path, X_OK) == 0)
+				{
+					strcpy(input, cmd_path);
+					break;
+				}
+				path_token = strtok(NULL, ":");
+			}
+		}
+		pid_t pid = fork();  /* Create a child process */
+		
+		if (pid == -1)
+		{
+			perror("Error: Unable to create a child process.");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			/* Child process */
+			argc = 0;
+			arguments[argc] = strtok(input, " ");  /* Split input into tokens */
+			while (arguments[argc] != NULL)
+			{
+				argc++;
+				arguments[argc] = strtok(NULL, " ");
+			}
+			arguments[argc] = NULL;
+			if (execvp(arguments[0], arguments) == -1)
+			{
+				perror("Execution error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			int status;
+			wait(&status);  /* Wait for child process to complete */
+		}
+	}
+	return 0;  /* Return 0 on success */
 }
