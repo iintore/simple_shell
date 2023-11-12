732be606a9ef1438cde61f865b131503a96d7ee5
Ana123tase <anastasedukunda@gmail.com>
2023-11-12 01:47:17 -0800

    A lovely shell

--- task2.c
+++ task2.c
@@ -8,61 +8,104 @@
 #define MAX_INPUT_LENGTH 100
 #define MAX_ARGUMENTS 20
 
+void displayPrompt(void);
+void getInput(char *input);
+void parseInput(char *input, char **arguments, int *argc);
+void executeCommand(char **arguments);
+
 /**
- * main - Simple Shell 0.2
- *
- * Descriptption:
- * It displays a prompt, waits for user input, and execute
- *
- * Return: 0 on success, -1 on error.
+ * main - accepts and processes in puts
+ * Return: int
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH];
-	char *arguments[MAX_ARGUMENTS];
-	int argc;
+char input[MAX_INPUT_LENGTH];
+char *arguments[MAX_ARGUMENTS];
+int argc;
 
-	while (1)
-	{
-		printf("shell$ ");
-		fgets(input, sizeof(input), stdin);
-		if (feof(stdin))
-		{
-			printf("\n");
-			break;  /* Exit on Ctrl+D (EOF) */
-		}
-		input[strcspn(input, "\n")] = '\0';
-		pid_t pid = fork();
+while (1)
+{
+displayPrompt();
+getInput(input);
 
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else
-			if (pid == 0)
-		{
-			/* Child process */
-			argc = 0;
-			arguments[argc] = strtok(input, " ");
-			while (arguments[argc] != NULL)
-			{
-				argc++;
-				arguments[argc] = strtok(NULL, " ");
-			}
-			arguments[argc] = NULL;
-			execvp(arguments[0], arguments);
-			perror(arguments[0]);
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			/* Parent process */
-			int status;
-
-			wait(&status);
-		}
-	}
-	return (0);
+if (feof(stdin))
+printf("\n");
+break; /* Exit on Ctrl+D (EOF) */
 }
 
+input[strcspn(input, "\n")] = '\0';
+
+pid_t pid = fork();
+
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+/* Child process */
+argc = 0;
+parseInput(input, arguments, &argc);
+executeCommand(arguments);
+}
+else
+{
+/* Parent process */
+int status;
+wait(&status);
+}
+return (0);
+}
+
+/**
+ * displayPrompt - displays shell $
+ * Return: northing
+ */
+void displayPrompt(void)
+{
+printf("shell$ ");
+}
+
+/**
+ * getInput - accepts user commands
+ * @input: user inputs
+ * Return: northing
+ */
+void getInput(char *input)
+{
+fgets(input, sizeof(input), stdin);
+}
+
+/**
+ * parseInput - shell parsing
+ * @input: user input
+ * @arguments: user args
+ * @argc: shell arguments
+ * Return: no return
+ */
+void parseInput(char *input, char **arguments, int *argc)
+{
+*argc = 0;
+arguments[*argc] = strtok(input, " ");
+
+while (arguments[*argc] != NULL)
+{
+(*argc)++;
+arguments[*argc] = strtok(NULL, " ");
+}
+
+arguments[*argc] = NULL;
+}
+
+/**
+ * executeCommand - find and execute executable files
+ * @arguments: user executable commands
+ * Return: Execurion
+ */
+void executeCommand(char **arguments)
+{
+execvp(arguments[0], arguments);
+perror(arguments[0]);
+exit(EXIT_FAILURE);
+}
